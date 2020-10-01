b57f6a1a9942c7db305f7ad9956224ebbeead2e5
DJdragonMVT <61909277+DJdragonMVT@users.noreply.github.com>
2020-10-01 16:51:15 -0500

    mas funciones

--- 3-Funciones.js
+++ 3-Funciones.js
@@ -13,10 +13,27 @@ var saludarv2 = function (){
 saludarv2();
 
 
-//-------
+//-------Parametro
 
 function suma(a,b){
     return a + b;
 }
 
 console.log(suma(5,10));
+
+function saludarEstudiante (nombreEstudiante){
+    console.log("Hola estudiante "+nombreEstudiante);
+}
+
+saludarEstudiante("Jose");
+
+function saludarEstudiante(nombreEstudiante){
+    console.log(` Hola estudiente ${nombreEstudiante}, te recuerdo que el nombre es ${nombreEstudiante}`) 
+}
+
+saludarEstudiante("Jose");
+
+function sumar (a,b){
+    var suma = a+b;
+    return suma;
+}
