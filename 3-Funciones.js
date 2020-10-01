c6ae8c91ad8444fd01299ce6c0466b312a41cbcc
DJdragonMVT <61909277+DJdragonMVT@users.noreply.github.com>
2020-10-01 16:49:33 -0500

    se Implementaron las Funciones

---
+++ 3-Funciones.js
@@ -0,0 +1,22 @@
+//declarativas
+
+function saludar(){
+    console.log("Hola")
+}
+
+//Expresion
+
+var saludarv2 = function (){
+    console.log("Hola soy la funcion 2")    
+}
+
+saludarv2();
+
+
+//-------
+
+function suma(a,b){
+    return a + b;
+}
+
+console.log(suma(5,10));
