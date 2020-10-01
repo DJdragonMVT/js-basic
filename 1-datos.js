0fa3e34383334c3e133021542457186d0fec8d30
DJdragonMVT <61909277+DJdragonMVT@users.noreply.github.com>
2020-10-01 16:32:33 -0500

    Se crearon las Funciones

---
+++ 1-datos.js
@@ -0,0 +1,24 @@
+//numeros
+40
+
+//texto
+"uy"
+'uy'
+
+
+//boolean
+true
+false
+
+
+null
+undefined
+
+//areglo
+[1,2.3];
+
+//objeto
+{
+    nombre: "uy";
+}
+
---
+++ 2-variables.js
@@ -0,0 +1,41 @@
+var nombre = "yo jeje";
+
+console.log("Hola hoy..." + nombre);
+
+//Declarar
+var edad;
+console.log(edad);
+
+//inicializar
+edad = 30;
+console.log(edad);
+
+
+//Var -> Reinicializar y reasignar
+var otro = "Hola";
+var otro = "Hola21";
+
+console.log(otro);
+
+
+//let -> no se reinicializa pero si se puede reasignar.
+let x=0;
+x=1;
+console.log(x);
+
+//Const -> no se reinicializa y no se reasigna.
+const z = 50;
+console.log(z);
+
+
+
+//-----
+var vegetales = ["tomate", "Lechuga" , "Zanahoria"]; 
+
+var persona = {
+    nombre = "Yo jeje",
+    edad = 19
+}
+
+console.log(vegetales);
+console.log(persona);
