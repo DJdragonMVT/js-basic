5710fe5e1043d4bc014e110f7ad32be12b590baa
DJdragonMVT <61909277+DJdragonMVT@users.noreply.github.com>
2020-10-05 15:48:15 -0500

    Creacion de Hosting

---
+++ 5-hosting.js
@@ -0,0 +1,21 @@
+var minombre;
+minombre = "yo";
+
+var minombre = "uy";
+
+//------
+console.log(apellido);
+var apellido = "avefr";
+
+//-------
+apellido = "averf";
+console.log(apellido);
+var apellido;
+
+
+//-----
+hola()
+
+function hola(){
+    console.log("hola")
+}
