970498c4d3caf5e14bf7d9e72d879e2ad835b920
DJdragonMVT <61909277+DJdragonMVT@users.noreply.github.com>
2020-10-05 16:06:58 -0500

    creacion truthy and falsy

---
+++ 7-truthy-and-falsy.js
@@ -0,0 +1,16 @@
+//falso
+
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(NaN)); //not a number
+console.log(Boolean(undefined));
+console.log(Boolean(false));
+console.log(Boolean(""));
+
+
+//Verdadero
+console.log(Boolean(1));
+console.log(Boolean("df"));
+console.log(Boolean([]));
+console.log(Boolean({}));
+console.log(Boolean(function(){}));
