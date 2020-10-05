4dec216dd26d79e684bf52a5b415baa9bcbccadd
DJdragonMVT <61909277+DJdragonMVT@users.noreply.github.com>
2020-10-05 16:29:42 -0500

    Creacion de If

---
+++ 9- if.js
@@ -0,0 +1,11 @@
+//Estrutura
+
+var haceSol = true;
+
+var estaLloviendo = false;
+
+if(haceSol || estaLloviendo){
+    console.log("Voy a sacar la Sombrilla")
+}else{
+    console.log("No voy a sacar la sombrilla")
+}
