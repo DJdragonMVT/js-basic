10ad27fa18ecfe8fdb9582208159780864211c93
DJdragonMVT <61909277+DJdragonMVT@users.noreply.github.com>
2020-10-08 16:18:22 -0500

    Arrays

---
+++ 11-arrays.js
@@ -0,0 +1,60 @@
+
+var juguetes = ["muñeca","carro","balon"];
+console.log(juguetes);
+
+//Pueden guardar mas de un tipo de dato
+var ejemplo = [1, "hola", "🎈","✨",{name : "javier", age: 21},true];
+console.log(ejemplo);
+
+
+//Cuantos elementos
+var juguetes = ["muñeca","carro","balon"];
+console.log(juguetes.length);
+
+
+//indice
+//                0         1       2
+var juguetes = ["muñeca","carro","balon"];
+console.log(juguetes[2]);
+
+//Push --> Agregar a la lista un objeto al final
+juguetes.push("Lego");
+console.log(juguetes);
+
+//pop --> Saca el ultimo elemento del arreglo
+juguetes.pop();
+console.log(juguetes);
+
+//unshift --> Agregar elementos al inicio de la lista
+juguetes.unshift("Lego");
+console.log(juguetes);
+
+//shift 
+juguetes.shift();
+console.log(juguetes);
+
+//lifo - fifo
+//pilas - folas(colas)
+
+//indexOf --> posiciones donde se encuentra un elemento
+var juguetes = ["muñeca","carro","balon"];
+var indexCarro = juguetes.indexOf("carro");
+console.log(indexCarro);
+
+//metodos adicionales
+var juguetes = ["muñeca","carro","balon"];
+juguetes.reverse(); //girar toda la lista
+console.log(juguetes);
+
+//sort --Z ordena la lista
+var juguetes = ["muñeca","carro","balon",1,"tractor",6,"lego",true,3];
+juguetes.sort();
+console.log(juguetes);
+
+//slice --> corta la lista entre las posicoines indicadas
+var juguetes = ["muñeca","carro","balon",1,"tractor",6,"lego",true,3];
+var recorte = juguetes.slice(0,3);
+console.log(recorte);
+
+
+
