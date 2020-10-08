9c0259e5038c505cafa03d763f5929affe1843c3
DJdragonMVT <61909277+DJdragonMVT@users.noreply.github.com>
2020-10-08 16:43:17 -0500

    Loops

---
+++ 12-Loops.js
@@ -0,0 +1,21 @@
+//For
+//variable,condicion,incremento
+
+for (var i = 0 ; i < 5 ; 1++ ){
+    //codigo
+}
+
+
+//for of (arreglos)
+for (var invitadoActual of invitados){
+
+}
+
+//While
+while (invitados.length > 0){
+
+}
+
+do{
+
+}while (pasoActual < numPasos)
